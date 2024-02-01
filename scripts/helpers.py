2b74ae2d356a7fb8f780c25628267bbb439f2da3
C4Wiz <C4Wiz@live.com>
2024-02-01 10:26:23 -0600

    ReSync

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -2,21 +2,8 @@ import xbmc
 import xbmcvfs
 import xbmcgui
 import sys
-import json
 
 units = [' Bytes', ' kB', ' MB', ' GB', ' TB']
-
-
-def jsonrpc(query):
-    querystring = {"jsonrpc": "2.0", "id": 1}
-    querystring.update(query)
-    try:
-        response = json.loads(xbmc.executeJSONRPC(json.dumps(querystring)))
-        if 'result' in response: return response['result']
-    except TypeError as e:
-        xbmc.log('Error executing JSON RPC: {}'.format(e.args), xbmc.LOGERROR)
-    return None
-
 
 if __name__ == '__main__':
     try:
@@ -26,17 +13,8 @@ if __name__ == '__main__':
                 argv[2]: Addon-Id
                 argv[3]: Enabled (true/false)
             '''
-            result = jsonrpc({"method": "Addons.SetAddonEnabled",
-                              "params": {"addonid": sys.argv[2], "enabled": bool(sys.argv[3])}})
-
-        elif sys.argv[1] == "getKodiSetting":
-            '''
-                get Kodi setting
-                [argv2]: setting e.g. "lookandfeel.skin" in guisettings.xml 
-            '''
-            result = jsonrpc({"method": "Settings.GetSettingValue", "params": {"setting": sys.argv[2]}})
-            xbmcgui.Window(10000).setProperty(sys.argv[2], str(result['value']))
-            xbmc.log('set Property \'%s\' to %s' % (sys.argv[2], str(result['value'])), xbmc.LOGINFO)
+            jsonrpc = '{"jsonrpc": "2.0", "method": "Addons.SetAddonEnabled", "params": {"addonid": "%s", "enabled": %s}, "id": 1}' % (sys.argv[2], sys.argv[3])
+            xbmc.executeJSONRPC(jsonrpc)
 
         elif sys.argv[1] == 'getFileSize':
             '''
@@ -54,34 +32,6 @@ if __name__ == '__main__':
             xbmcgui.Window(10000).setProperty('size', '%s%s' % ('{0:0.2f}'.format(fs), units[unit]))
             xbmc.log('set Property \'size\' to %s%s' % ('{0:0.2f}'.format(fs), units[unit]), xbmc.LOGINFO)
 
-        elif sys.argv[1] == 'calculate':
-            '''
-                calculates two parameters, use integer operations
-                [argv2]: operator (add, sub, div, mul)
-                [argv3]: first operand (float)
-                [argv4]: second operand (float)
-                [argv5]: property
-                returns result (int) in property Window(Home).getProperty(property)
-            '''
-            try:
-
-                p1 = float(sys.argv[3])
-                p2 = float(sys.argv[4])
-                operator = sys.argv[2]
-                prop = sys.argv[5]
-
-                if operator == 'add': xbmcgui.Window(10000).setProperty(prop, str(int(p1 + p2)))
-                elif operator == 'sub': xbmcgui.Window(10000).setProperty(prop, str(int(p1 - p2)))
-                elif operator == 'mul': xbmcgui.Window(10000).setProperty(prop, str(int(p1 * p2)))
-                elif operator == 'div': xbmcgui.Window(10000).setProperty(prop, str(int(p1 / p2)))
-                else: raise ValueError('Operator unknown')
-
-                xbmc.log('Property \'%s\' calculated' % prop, xbmc.LOGINFO)
-
-            except IndexError:
-                xbmc.log('not all parameters provided for math operations', xbmc.LOGERROR)
-            except ValueError as e:
-                xbmc.log('Value error: %s' % str(e), xbmc.LOGERROR)
         else:
             xbmc.log('unknown parameter', xbmc.LOGERROR)
 
